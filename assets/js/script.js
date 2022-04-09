70afa1d6c4986b774723ec611022d35b76961893
Sam Wallace <sam@sammymayhem.com>
2022-04-08 22:32:34 -0700

    get weather api function works now and shows data on page when called

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,45 @@
 var apiKey = "72dcba6758c0f3fef0a894c324ffa5fc";
 
-function getWeaterApi() {
-    var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=" + apiKey;
+var todayContainer = document.getElementById("weather-today");
+var fetchButton = document.getElementById("fetch");
 
-    fetch(requestUrl).then(function(response) {
-        return response.json();
-    }).then(function (data) {
-        console.log(data);
-    })
+function getWeaterApi() {
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=San+Diego&exclude=minutely&units=imperial&appid=" + apiKey;
+
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            var cityData = document.createElement("h2");
+            var tempData = document.createElement("p");
+            var windData = document.createElement("p");
+            var humidityData = document.createElement("p");
+            var uvData = document.createElement("p");
+
+            cityData.textContent = data.name;
+            tempData.textContent = data.main.temp;
+            windData.textContent = data.wind.speed;
+            humidityData.textContent = data.main.humidity;
+
+            console.log(cityData);
+
+            todayContainer.append(cityData);
+            todayContainer.append("Temp: " + tempData);
+            todayContainer.append("Wind Speed: " + windData);
+            todayContainer.append("Humidity: " + humidityData);
+
+        }
+        )
 }
 
-getWeaterApi();
+fetchButton.addEventListener('click', getWeaterApi);
+
+
+// city (current date)
+// temp F
+// wind
+// humidty
+// uv rating
