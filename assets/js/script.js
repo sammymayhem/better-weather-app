e856462e11a9e8f47f583a5d60d8fe81e9286845
Sam Wallace <sam@sammymayhem.com>
2022-04-09 11:17:25 -0700

    Updated layout to include search and included date next to city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 var apiKey = "72dcba6758c0f3fef0a894c324ffa5fc";
 
+var currentDate = moment().format("l");
+
 var todayContainer = document.getElementById("weather-today");
 var fetchButton = document.getElementById("fetch");
 
+// Fetch informatin from weather API
 function getWeaterApi() {
     var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=San+Diego&exclude=minutely&units=imperial&appid=" + apiKey;
 
@@ -19,20 +22,17 @@ function getWeaterApi() {
             var humidityData = document.createElement("p");
             var uvData = document.createElement("p");
 
-            cityData.textContent = data.name;
-            tempData.textContent = data.main.temp;
-            windData.textContent = data.wind.speed;
-            humidityData.textContent = data.main.humidity;
-
-            console.log(cityData);
+            cityData.textContent = data.name + " (" + currentDate + ")";
+            tempData.textContent = "Temp: " + data.main.temp;
+            windData.textContent = "Wind: " + data.wind.speed;
+            humidityData.textContent = "Humidity: " + data.main.humidity;
 
             todayContainer.append(cityData);
             todayContainer.append(tempData);
             todayContainer.append(windData);
             todayContainer.append(humidityData);
 
-        }
-        )
+        });
 }
 
 fetchButton.addEventListener('click', getWeaterApi);
