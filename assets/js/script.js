05e2eebb2d2a206da7defc4bb3396201265ff508
Sam Wallace <sam@sammymayhem.com>
2022-04-08 20:21:07 -0700

    added weather api get function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
-* {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
+var apiKey = "72dcba6758c0f3fef0a894c324ffa5fc";
+
+function getWeaterApi() {
+    var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=" + apiKey;
+
+    fetch(requestUrl).then(function(response) {
+        return response.json();
+    }).then(function (data) {
+        console.log(data);
+    })
 }
 
-#top {
-    
-}
+getWeaterApi();
